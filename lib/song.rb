47a0061283310c3f83f1b2f80e652c41cdea5e89
Lia Skalkos <hello@liaskalkos.net>
2015-06-14 14:31:56 -0400

    Complete tests and all passing

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,14 @@
 # code song here
 
 class Song
+  attr_accessor :name, :artist
   attr_reader :genre
+
 
   def genre=(genre)
     genre.add_song(self)
     @genre = genre
   end
+  
 
 end
--- spec/song_spec.rb
+++ spec/song_spec.rb
@@ -1,17 +1,32 @@
 describe "Song" do
-  xit "can initialize a song" do
+  it "can initialize a song" do
      # implement this spec
+    expect(Song.new).to be_an_instance_of(Song)    
   end
 
-  xit "can have a name" do
+  it "can have a name" do
      # implement this spec
+     song = Song.new
+     song.name = "Twinkle Twinkle Little Star"
+      expect(song.name).to eq("Twinkle Twinkle Little Star")
   end
 
-  xit "can have a genre" do
+  it "can have a genre" do
      # implement this spec
+     song = Song.new
+     genre = Genre.new
+     genre.name = "House"
+     song.genre = genre
+     expect(song.genre.name).to eq("House")
   end
 
-  xit "has an artist" do
+  it "has an artist" do
      # implement this spec
+      artist = Artist.new
+      artist.name = "Ke$ha"
+      song = Song.new
+      song.artist = artist
+      expect(song.artist.name).to eq("Ke$ha")
+
   end
 end
